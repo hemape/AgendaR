c2189a221932c0b6a1f659bf9748e53abe84f9c9
hemape <hectormp97@gmail.com>
2018-05-14 19:04:24 +0200

    Recylcer Adapter

---
+++ app/src/main/java/com/example/hector/agendarecuperacion/AdapterRecyler.java
@@ -0,0 +1,56 @@
+package com.example.hector.agendarecuperacion;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class AdapterRecyler extends RecyclerView.Adapter<AdapterRecyler.DiasTareasHolder> {
+
+    List<Evento> eventos;
+
+    public AdapterRecyler(List<Evento>eventos){
+        this.eventos = eventos;
+
+    }
+
+
+    @Override
+    public DiasTareasHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.linea_recylcer, parent, false);
+        DiasTareasHolder holder = new DiasTareasHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiasTareasHolder holder, int position) {
+        Evento evento =  eventos.get(position);
+        holder.textViewNombre.setText(evento.getNombre());
+        holder.textViewDiaTarea.setText(evento.getFecha());
+        holder.textViewHoraTarea.setText(evento.getHora());
+        holder.textViewDescripcionTarea.setText(evento.getDescripcion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventos.size();
+    }
+
+    public static class DiasTareasHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewNombre, textViewDiaTarea, textViewHoraTarea, textViewDescripcionTarea;
+        public DiasTareasHolder(View itemView) {
+            super(itemView);
+            textViewNombre = itemView.findViewById(R.id.textview_nombrerecy);
+            textViewDiaTarea = itemView.findViewById(R.id.textview_fecharecy);
+            textViewHoraTarea = itemView.findViewById(R.id.textview_horarecy);
+            textViewDescripcionTarea = itemView.findViewById(R.id.textview_descripcionrecy);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/hector/agendarecuperacion/RecyclerActivity.java
@@ -0,0 +1,19 @@
+package com.example.hector.agendarecuperacion;
+
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+
+
+public class RecyclerActivity  {
+    RecyclerView rv;
+    List<Evento>eventos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setConte
+    }
+}
